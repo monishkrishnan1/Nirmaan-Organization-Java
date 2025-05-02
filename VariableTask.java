f88a7718d0580acb2fe0136909e69afac00f93eb
monishkrishnan1 <mkmonishkrish@gmail.com>
2025-05-02 11:31:44 +0530

    Add files via upload

---
+++ VariableTask.java
@@ -0,0 +1,26 @@
+package Day_15;
+
+public class VariableTask {
+
+	public static void main(String[] args) {
+		
+		byte byt = 127;
+		short sht = 30000;
+		int inte = 200000;
+		long lng = 9940174459l;
+		float flt = 5.123f;
+		double dbl = 1.2345678;
+		char chr = 'A';
+		boolean boo = true;
+		
+		System.out.println("Byte : "+byt);
+		System.out.println("Short : "+sht);
+		System.out.println("Integer : "+inte);
+		System.out.println("Long : "+lng);
+		System.out.println("Float : "+flt);
+		System.out.println("Double : "+dbl);
+		System.out.println("Character : "+chr);
+		System.out.println("Boolean : "+boo);
+	}
+
+}
